4753453bc560e711845051e300789cd384aa566b
Bruno Girome <brunogirome@gmail.com>
2020-03-27 08:16:58 -0300

    Checagem de estoque e refatoração do reducer

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,19 +1,50 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, select, put, all, takeLatest } from 'redux-saga/effects';
 import api from '../../../services/api';
+import { formatPrice } from '../../../util/format';
 
-import { addToCartSucess } from './actions';
+import { addToCartSucess, updateAmount } from './actions';
 
 // O * tem uma função semelhante a do async, porém, com algumas funcionalidades
 // extras. O nome dessa feature é 'Generator'
 function* addToCart({ id }) {
-  // - yield é equivalente ao await
-  // - call é um método do redux saga que gerencia outros métodos assíncronos
-  //   Detalhe: no call, o primeiro parâmetro é a função que será executada,
-  // e os demais são os parâmetros da função que foi passada
-  const response = yield call(api.get, `/products/${id}`);
+  // Todo effects do saga precisa de um yield
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
 
-  // O put dispara uma action do Redux
-  yield put(addToCartSucess(response.data));
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    console.tron.warn('ERROR');
+    return;
+  }
+
+  if (productExists) {
+    // Lembrando que o put dispara actions do Redux. Essas actions foram
+    // programadas em: /src/store/modules/cart/actions.js, e são importadas
+    // e disparadas aqui no put
+    yield put(updateAmount(id, amount));
+  } else {
+    // - yield é equivalente ao await
+    // - call é um método do redux saga que gerencia outros métodos assíncronos
+    //   Detalhe: no call, o primeiro parâmetro é a função que será executada,
+    // e os demais são os parâmetros da função que foi passada
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+
+    // O put dispara uma action do Redux
+    yield put(addToCartSucess(data));
+  }
 }
 
 // Actions que esse 'middleware' vai escutar
