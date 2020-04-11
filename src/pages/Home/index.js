2850104bce9fae9ea174292e97259f769ab6f13e
Bruno Girome <brunogirome@gmail.com>
2020-04-11 19:50:04 -0300

    Using useDispatch and useSelector from Redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
@@ -9,8 +8,16 @@ import * as CartActions from '../../store/modules/cart/actions';
 
 import { ProductList } from './styles';
 
-function Home({ amount, addToCartRequest }) {
+export default function Home() {
   const [products, setProducts] = useState([]);
+  const amount = useSelector(state =>
+    state.cart.reduce((sumAmount, product) => {
+      sumAmount[product.id] = product.amount;
+      return sumAmount;
+    }, {})
+  );
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
     async function loadProducts() {
@@ -28,7 +35,7 @@ function Home({ amount, addToCartRequest }) {
   }, []);
 
   function handleAddProduct(id) {
-    addToCartRequest(id);
+    dispatch(CartActions.addToCartRequest(id));
   }
 
   return (
@@ -50,30 +57,3 @@ function Home({ amount, addToCartRequest }) {
     </ProductList>
   );
 }
-
-const mapStateToProps = state => ({
-  // Reduce: faz que com um vetor retorne apenas um valor tradando os dados
-  // do mesmo
-  // - amount: valor que será retornado. Ele é iniciado pelo valor que vai como
-  // segundo parâmetro da função
-  // - product: valor da item atual do array
-  //
-  // Detalhe: amount[product.id] é uma maneira de atribuir uma chave a um item
-  // do objeto como um número, exemplo:
-  //
-  // const Yumi = { id: 13, name: 'Yumi'};
-  // Yumi[7] = 'Key as value';
-  //
-  // Neste caso, objeto Yumi receberá uma propriedade "7" com o valor
-  // 'Key as value'. Se eu tentar acessar Yumi[7], vou receber a tal string
-  // como retorno
-  amount: state.cart.reduce((amount, product) => {
-    amount[product.id] = product.amount;
-    return amount;
-  }, {}),
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(CartActions, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
